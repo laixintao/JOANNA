f8e409b300b8ba3338e3663b09c378dcc7f1c0bf
赖信涛 <laixintao1995@163.com>
2015-08-01 12:29:29 +0800

    use pointer,drop write_mem8

--- SHANA/bootpack.c
+++ SHANA/bootpack.c
@@ -4,9 +4,11 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
-
+	char *p;
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f); /* MOV BYTE [i],15 */
+		//write_mem8(i, i & 0x0f); /* MOV BYTE [i],15 */
+		p = i;
+		*p = i & 0x0f;
 	}
 
 	for (;;) {
