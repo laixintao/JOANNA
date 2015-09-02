4c1c84194921a4d8df8d6a73f5051223be453505
赖信涛 <laixintao1995@163.com>
2015-09-02 15:28:47 +0800

    图层的叠加处理

--- JOANNA/sheet.c
+++ JOANNA/sheet.c
@@ -47,37 +47,6 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
 	return;
 }
 
-void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
-{
-	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
-	unsigned char *buf, c, *vram = ctl->vram;
-	struct SHEET *sht;
-	for (h = 0; h <= ctl->top; h++) {
-		sht = ctl->sheets[h];
-		buf = sht->buf;
-		/* vx0～vy1を使って、bx0～by1を逆算する */
-		bx0 = vx0 - sht->vx0;
-		by0 = vy0 - sht->vy0;
-		bx1 = vx1 - sht->vx0;
-		by1 = vy1 - sht->vy0;
-		if (bx0 < 0) { bx0 = 0; }
-		if (by0 < 0) { by0 = 0; }
-		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
-		if (by1 > sht->bysize) { by1 = sht->bysize; }
-		for (by = by0; by < by1; by++) {
-			vy = sht->vy0 + by;
-			for (bx = bx0; bx < bx1; bx++) {
-				vx = sht->vx0 + bx;
-				c = buf[by * sht->bxsize + bx];
-				if (c != sht->col_inv) {
-					vram[vy * ctl->xsize + vx] = c;
-				}
-			}
-		}
-	}
-	return;
-}
-
 void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 {
 	int h, old = sht->height; /* 設定前の高さを記憶する */
@@ -110,7 +79,7 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 			}
 			ctl->top--; /* 表示中の下じきが一つ減るので、一番上の高さが減る */
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
+		sheet_refresh(ctl); /* 新しい下じきの情報に沿って画面を描き直す */
 	} else if (old < height) {	/* 以前よりも高くなる */
 		if (old >= 0) {
 			/* 間のものを押し下げる */
@@ -128,27 +97,39 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 			ctl->sheets[height] = sht;
 			ctl->top++; /* 表示中の下じきが一つ増えるので、一番上の高さが増える */
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
+		sheet_refresh(ctl); /* 新しい下じきの情報に沿って画面を描き直す */
 	}
 	return;
 }
 
-void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+void sheet_refresh(struct SHTCTL *ctl)
 {
-	if (sht->height >= 0) { /* もしも表示中なら、新しい下じきの情報に沿って画面を描き直す */
-		sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+	int h, bx, by, vx, vy;
+	unsigned char *buf, c, *vram = ctl->vram;
+	struct SHEET *sht;
+	for (h = 0; h <= ctl->top; h++) {
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+		for (by = 0; by < sht->bysize; by++) {
+			vy = sht->vy0 + by;
+			for (bx = 0; bx < sht->bxsize; bx++) {
+				vx = sht->vx0 + bx;
+				c = buf[by * sht->bxsize + bx];
+				if (c != sht->col_inv) {
+					vram[vy * ctl->xsize + vx] = c;
+				}
+			}
+		}
 	}
 	return;
 }
 
 void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
 {
-	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
 	sht->vx0 = vx0;
 	sht->vy0 = vy0;
-	if (sht->height >= 0) { /* もしも表示中なら、新しい下じきの情報に沿って画面を描き直す */
-		sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
-		sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+	if (sht->height >= 0) { /* もしも表示中なら */
+		sheet_refresh(ctl); /* 新しい下じきの情報に沿って画面を描き直す */
 	}
 	return;
 }
