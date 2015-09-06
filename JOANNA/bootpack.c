94fcb1d27b8e8d53136f50bfac52509dc2f12d15
赖信涛 <laixintao1995@163.com>
2015-09-06 10:35:40 +0800

    多任务代码重构以及优化

--- JOANNA/bootpack.c
+++ JOANNA/bootpack.c
@@ -14,7 +14,7 @@ struct TSS32 {
 	int ldtr, iomap;
 };
 
-void task_b_main(void);
+void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void)
 {
@@ -22,7 +22,7 @@ void HariMain(void)
 	struct FIFO32 fifo;
 	char s[40];
 	int fifobuf[128];
-	struct TIMER *timer, *timer2, *timer3, *timer_ts;
+	struct TIMER *timer, *timer2, *timer3;
 	int mx, my, i, cursor_x, cursor_c, task_b_esp;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
@@ -60,9 +60,6 @@ void HariMain(void)
 	timer3 = timer_alloc();
 	timer_init(timer3, &fifo, 1);
 	timer_settime(timer3, 50);
-	timer_ts = timer_alloc();
-	timer_init(timer_ts, &fifo, 2);
-	timer_settime(timer_ts, 2);
 
 	memtotal = memtest(0x00400000, 0xbfffffff);
 	memman_init(memman);
@@ -106,7 +103,7 @@ void HariMain(void)
 	set_segmdesc(gdt + 3, 103, (int) &tss_a, AR_TSS32);
 	set_segmdesc(gdt + 4, 103, (int) &tss_b, AR_TSS32);
 	load_tr(3 * 8);
-	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
+	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
 	tss_b.eip = (int) &task_b_main;
 	tss_b.eflags = 0x00000202; /* IF = 1; */
 	tss_b.eax = 0;
@@ -123,7 +120,8 @@ void HariMain(void)
 	tss_b.ds = 1 * 8;
 	tss_b.fs = 1 * 8;
 	tss_b.gs = 1 * 8;
-	*((int *) 0x0fec) = (int) sht_back;
+	*((int *) (task_b_esp + 4)) = (int) sht_back;
+	mt_init();
 
 	for (;;) {
 		io_cli();
@@ -132,10 +130,7 @@ void HariMain(void)
 		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
-			if (i == 2) {
-				farjmp(0, 4 * 8);
-				timer_settime(timer_ts, 2);
-			} else if (256 <= i && i <= 511) { /* キーボードデータ */
+			if (256 <= i && i <= 511) { /* キーボードデータ */
 				sprintf(s, "%02X", i - 256);
 				putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
 				if (i < 0x54 + 256) {
@@ -284,33 +279,38 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c)
 	return;
 }
 
-void task_b_main(void)
+void task_b_main(struct SHEET *sht_back)
 {
 	struct FIFO32 fifo;
-	struct TIMER *timer_ts;
-	int i, fifobuf[128], count = 0;
-	char s[11];
-	struct SHEET *sht_back;
+	struct TIMER *timer_put, *timer_1s;
+	int i, fifobuf[128], count = 0, count0 = 0;
+	char s[12];
 
 	fifo32_init(&fifo, 128, fifobuf);
-	timer_ts = timer_alloc();
-	timer_init(timer_ts, &fifo, 1);
-	timer_settime(timer_ts, 2);
-	sht_back = (struct SHEET *) *((int *) 0x0fec);
+	timer_put = timer_alloc();
+	timer_init(timer_put, &fifo, 1);
+	timer_settime(timer_put, 1);
+	timer_1s = timer_alloc();
+	timer_init(timer_1s, &fifo, 100);
+	timer_settime(timer_1s, 100);
 
 	for (;;) {
 		count++;
-		sprintf(s, "%10d", count);
-		putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
 			io_sti();
 		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
-			if (i == 1) { /* タスクスイッチ */
-				farjmp(0, 3 * 8);
-				timer_settime(timer_ts, 2);
+			if (i == 1) {
+				sprintf(s, "%11d", count);
+				putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 11);
+				timer_settime(timer_put, 1);
+			} else if (i == 100) {
+				sprintf(s, "%11d", count - count0);
+				putfonts8_asc_sht(sht_back, 0, 128, COL8_FFFFFF, COL8_008484, s, 11);
+				count0 = count;
+				timer_settime(timer_1s, 100);
 			}
 		}
 	}
